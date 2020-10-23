cb444d0ec7861c2c485e706e30b12ddfdd83d5ca
Sagar Sarkar <sagar_mon@hotmail.com>
2020-10-24 01:28:19 +0600

    Update array_front9.py

--- array_front9.py
+++ array_front9.py
@@ -1,3 +1,7 @@
+'''
+Given an array of ints, return True if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+'''
+
 def array_front9(nums):
   if len(nums) > 4:
     for i in range(4):
